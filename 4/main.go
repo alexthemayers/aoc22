e6a27b1c8e697fb649aef954a6e7f3115636803a
Alexander Mayers <62880419+alexthemayers@users.noreply.github.com>
2022-12-04 09:00:48 +0200

    day 4 part 2

--- 4/main.go
+++ 4/main.go
@@ -10,13 +10,6 @@ import (
 
 const input = "./4/input.txt"
 
-/*
-Every section has an id number
-each elf is assigned a range of sections
-
-some assignments overlap
-*/
-
 func main() {
 	// Open Input file
 	data, err := os.ReadFile(input)
@@ -38,4 +31,41 @@ func main() {
 	}
 	fmt.Printf("Part 1: %d assignment overlaps\n", assignmentOverlaps)
 
+	// Part 2
+	assignmentOverlaps = 0
+	for _, assignment := range assignments {
+		first := strings.Split(strings.Split(assignment, ",")[0], "-")
+		second := strings.Split(strings.Split(assignment, ",")[1], "-")
+		f0, _ := strconv.Atoi(first[0])
+		f1, _ := strconv.Atoi(first[1])
+		s0, _ := strconv.Atoi(second[0])
+		s1, _ := strconv.Atoi(second[1])
+		firstAssignment := makeSlice(f0, f1)
+		secondAssignment := makeSlice(s0, s1)
+
+		overlap := false
+		for _, f := range firstAssignment {
+			for _, s := range secondAssignment {
+				if f == s {
+					overlap = true
+				}
+			}
+		}
+		if overlap == true {
+			assignmentOverlaps++
+		}
+	}
+	fmt.Printf("Part 2: %d assignment overlaps\n", assignmentOverlaps)
+
+}
+
+func makeSlice(start, end int) []int {
+	if end < start {
+		panic("yikes")
+	}
+	s := make([]int, 0, end-start)
+	for i := start; i <= end; i++ {
+		s = append(s, i)
+	}
+	return s
 }
